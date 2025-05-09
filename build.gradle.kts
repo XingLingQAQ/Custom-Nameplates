70e84b9bfad4cd5d70f257345816228a6539285b
星凌 <92240364+XingLingQAQ@users.noreply.github.com>
2025-05-09 11:39:12 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.io.ByteArrayOutputStream
 
 plugins {
     id("java")
-    id("com.gradleup.shadow") version "9.0.0-beta9"
+    id("com.gradleup.shadow") version "9.0.0-beta13"
 }
 
 val git : String = versionBanner()
